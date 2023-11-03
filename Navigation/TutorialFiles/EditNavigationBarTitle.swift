4d47eedd6c6bad9af3d52251222c232178bfd36e
Scott Crowder <scrowder@gmail.com>
2023-11-03 16:19:09 -0500

    create file

---
+++ Navigation/TutorialFiles/EditNavigationBarTitle.swift
@@ -0,0 +1,18 @@
+//
+//  EditNavigationBarTitle.swift
+//  Navigation
+//
+//  Created by SCOTT CROWDER on 11/3/23.
+//
+
+import SwiftUI
+
+struct EditNavigationBarTitle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditNavigationBarTitle()
+}
