6fee4db5e5a9ada98829fd1b3e4e0f6fa42b6218
Scott Crowder <scrowder@gmail.com>
2023-11-03 09:44:01 -0500

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by SCOTT CROWDER on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
