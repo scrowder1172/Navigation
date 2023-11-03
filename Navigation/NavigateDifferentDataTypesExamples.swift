b9750faf42ba931e7a8f0db524d05c3b6e9cfce1
Scott Crowder <scrowder@gmail.com>
2023-11-03 13:24:51 -0500

    create examples of navigating using different data types

--- Navigation/NavigateDifferentDataTypesExamples.swift
+++ Navigation/NavigateDifferentDataTypesExamples.swift
@@ -7,12 +7,61 @@
 
 import SwiftUI
 
-struct NavigateDifferentDataTypesExamples: View {
+struct SimpleNavigationDifferentDataTypes: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                ForEach(0..<5) {i in
+                    NavigationLink("Select Number: \(i)", value: i)
+                }
+                
+                ForEach(0..<5) {i in
+                    NavigationLink("Select String: \(i)", value: String(i))
+                }
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected the number \(selection)")
+            }
+            .navigationDestination(for: String.self) { selection in
+                Text("You selected the string \(selection)")
+            }
+        }
+    }
+}
+
+struct ComplexNavigationDifferentDataTypes: View {
+    @State private var path: NavigationPath = NavigationPath()
+    
+    var body: some View {
+        NavigationStack(path: $path) {
+            List {
+                ForEach(0..<5) {i in
+                    NavigationLink("Select Number: \(i)", value: i)
+                }
+                
+                ForEach(0..<5) {i in
+                    NavigationLink("Select String: \(i)", value: String(i))
+                }
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected the number \(selection)")
+            }
+            .navigationDestination(for: String.self) { selection in
+                Text("You selected the string \(selection)")
+            }
+            .toolbar {
+                Button("Push 556") {
+                    path.append(556)
+                }
+                
+                Button("Push Hello") {
+                    path.append("Hello")
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    NavigateDifferentDataTypesExamples()
+    ComplexNavigationDifferentDataTypes()
 }
