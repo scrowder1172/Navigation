df94e0c150180fecafcaa455bedac6a95e71ecd1
Scott Crowder <scrowder@gmail.com>
2023-11-03 15:13:09 -0500

    Save path within NavigationStack

--- Navigation/SaveNavigationStackPath.swift
+++ Navigation/SaveNavigationStackPath.swift
@@ -7,9 +7,90 @@
 
 import SwiftUI
 
-struct SaveNavigationStackPath: View {
+@Observable
+class HomogenousPathStore {
+    var path: [Int] {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
+                path = decoded
+                return
+            }
+        }
+        
+        // set path to empty array if url can't be found or loaded
+        path = []
+    }
+    
+    func save() {
+        do {
+            let data = try JSONEncoder().encode(path)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
+@Observable
+class NavigationPathPathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        // set path to empty array if url can't be found or loaded
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
+struct SavePathDetailView: View {
+    var number: Int
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink("Go to a Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+    }
+}
+
+struct SaveNavigationStackPath: View {
+    @State private var pathStore: NavigationPathPathStore = NavigationPathPathStore()
+    
+    var body: some View {
+        NavigationStack(path: $pathStore.path) {
+            SavePathDetailView(number: 0)
+                .navigationDestination(for: Int.self) { i in
+                    SavePathDetailView(number: i)
+            }
+        }
     }
 }
 
