3f35fa83c2d84243817cba6c16c6b4fd3688d993
Scott Crowder <scrowder@gmail.com>
2023-11-03 16:18:45 -0500

    customize navigation bar buttons

--- Navigation/TutorialFiles/PlaceToolbarButtonsExactly.swift
+++ Navigation/TutorialFiles/PlaceToolbarButtonsExactly.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct PlaceToolbarButtonsExactly: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(1..<5) { i in
+                Text("Item #\(i)")
+            }
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Agree") {
+                        // do something
+                    }
+                }
+                
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        
+                    }
+                }
+                
+                ToolbarItemGroup(placement: .destructiveAction) {
+                    Button("GB1") {}
+                    Button("GB2") {}
+                }
+                
+            }
+        }
+        
     }
 }
 
