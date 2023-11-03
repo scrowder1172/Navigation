dd47154495ead6cbdffbd9e08370107289e2d438
Scott Crowder <scrowder@gmail.com>
2023-11-03 13:55:18 -0500

    create file

---
+++ Navigation/SaveNavigationStackPath.swift
@@ -0,0 +1,18 @@
+//
+//  SaveNavigationStackPath.swift
+//  Navigation
+//
+//  Created by SCOTT CROWDER on 11/3/23.
+//
+
+import SwiftUI
+
+struct SaveNavigationStackPath: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SaveNavigationStackPath()
+}
