2b1287c093a22c5131eedb7cb7a0292991551c96
Scott Crowder <scrowder@gmail.com>
2023-11-03 13:06:01 -0500

    create file

---
+++ Navigation/NavigateDifferentDataTypesExamples.swift
@@ -0,0 +1,18 @@
+//
+//  NavigateDifferentDataTypesExamples.swift
+//  Navigation
+//
+//  Created by SCOTT CROWDER on 11/3/23.
+//
+
+import SwiftUI
+
+struct NavigateDifferentDataTypesExamples: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigateDifferentDataTypesExamples()
+}
