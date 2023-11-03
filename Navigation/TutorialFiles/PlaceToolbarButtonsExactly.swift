87283b94c7d374f991c85f4d331242780e73cadc
Scott Crowder <scrowder@gmail.com>
2023-11-03 16:02:09 -0500

    create file

---
+++ Navigation/TutorialFiles/PlaceToolbarButtonsExactly.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceToolbarButtonsExactly.swift
+//  Navigation
+//
+//  Created by SCOTT CROWDER on 11/3/23.
+//
+
+import SwiftUI
+
+struct PlaceToolbarButtonsExactly: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaceToolbarButtonsExactly()
+}
