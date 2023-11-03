2e44712e929ad19840037dd23ed4c42c1ed323cd
Scott Crowder <scrowder@gmail.com>
2023-11-03 12:13:34 -0500

    created navigation link examples

--- Navigation/NavigationLinkExamples.swift
+++ Navigation/NavigationLinkExamples.swift
@@ -9,6 +9,23 @@ import SwiftUI
 
 struct NavigationLinkExamples: View {
     var body: some View {
+//        NavigationStack {
+//            NavigationLink("Simple") {
+//                SimpleNavigationLinkExample()
+//            }
+//            NavigationLink("Dynamic") {
+//                DynamicNavigationLinkExample()
+//            }
+//            NavigationLink("Advance") {
+//                AdvancedNavigationLinkExample()
+//            }
+//        }
+        AdvancedNavigationLinkExample()
+    }
+}
+
+struct SimpleNavigationLinkExample: View {
+    var body: some View {
         NavigationStack {
             NavigationLink("Tap Me") {
                 Text("Detail View")
@@ -17,6 +34,77 @@ struct NavigationLinkExamples: View {
     }
 }
 
+struct DynamicNavigationLinkExample: View {
+    var body: some View {
+        NavigationStack {
+            NavigationLink("Tap Me") {
+                DetailView(number: 556)
+            }
+        }
+        NavigationStack {
+            List(0..<1000) { i in
+                NavigationLink("Tap Me #\(i)") {
+                    DetailView(number: i)
+                }
+            }
+        }
+
+    }
+}
+
+struct DetailView: View {
+    var number: Int
+    
+    var body: some View {
+         Text("Detail View: \(number)")
+    }
+    
+    init(number: Int) {
+        self.number = number
+        print("Creating detail view #\(number)")
+    }
+}
+
+struct AdvancedNavigationLinkExample: View {
+    var body: some View {
+        NavigationStack {
+            List(0..<100) {i in
+                NavigationLink("Select \(i)", value: i)
+                NavigationLink("Go to Detail View", value: "Detail")
+                NavigationLink("Get Info On User", value: "Robert Smith")
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected \(selection)")
+            }
+            .navigationDestination(for: Student.self) {student in
+                Text("Welcome, \(student.name)")
+            }
+            .navigationDestination(for: String.self) {text in
+                AdvancedDetailView(text: text)
+            }
+        }
+    }
+}
+
+struct Student: Hashable {
+    var uuid:UUID = UUID()
+    var name: String
+    var age: Int
+}
+
+struct AdvancedDetailView: View {
+    let text: String
+    var body: some View {
+        Text("You are viewing the \(text)")
+    }
+    
+    init(text: String) {
+        self.text = text
+        print("Display: \(text)")
+    }
+}
+
 #Preview {
     NavigationLinkExamples()
+    //AdvancedNavigationLinkExample()
 }
