13139ad738fb2760263df20900a428ab6942fd89
Scott Crowder <scrowder@gmail.com>
2023-11-03 12:14:03 -0500

    create file

---
+++ Navigation/ProgrammaticNavigation.swift
@@ -0,0 +1,18 @@
+//
+//  ProgrammaticNavigation.swift
+//  Navigation
+//
+//  Created by SCOTT CROWDER on 11/3/23.
+//
+
+import SwiftUI
+
+struct ProgrammaticNavigation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgrammaticNavigation()
+}
